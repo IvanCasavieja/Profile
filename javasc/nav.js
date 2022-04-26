6bf79006e947c354446deb5e038aa33f7f830427
Ivan Casavieja <ivanmichelcasavieja@gmail.com>
2022-04-26 01:24:26 -0300

    navjs y animation

--- javasc/nav.js
+++ javasc/nav.js
@@ -1,11 +1,6 @@
-let ubicacionPrincipal  = window.pageYOffset;
-window.onscroll = function() {
-    let Desplazamiento_Actual = window.pageYOffset;
-    if(ubicacionPrincipal >= Desplazamiento_Actual){
-        document.getElementById('nav').style.top = '0';
-    }
-    else{
-        document.getElementById('nav').style.top = '-100px';
-    }
-    ubicacionPrincipal = Desplazamiento_Actual;
+window.onscroll = function () {
+  let offsetNav = 60;
+  const secciones = document.getElementsByTagName("section");
+  document.getElementById("nav").style.top = "-100px";
+
 }
