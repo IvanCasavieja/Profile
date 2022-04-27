4491382de63898189d9de23c370f7ba4a3da3f17
Ivan Casavieja <ivanmichelcasavieja@gmail.com>
2022-04-26 22:03:36 -0300

    animaciones

--- javasc/conoceme.js
+++ javasc/conoceme.js
@@ -1,10 +1,10 @@
 class infoPersonaObject {
-    constructor(nameCalled, age, razon, region) {
-        this.nameCalled = nameCalled;
-        this.age = age;
-        this.razon = razon;
-        this.region = region;
-    }
+  constructor(nameCalled, age, razon, region) {
+    this.nameCalled = nameCalled;
+    this.age = age;
+    this.razon = razon;
+    this.region = region;
+  }
 }
 const infoPersona = [];
 
@@ -17,43 +17,54 @@ console.log(conversar.innerHTML);
 
 const txtConversar = document.getElementById("txt-conversar");
 let infoPersonaArray = []; //declarar array
-txtConversar.addEventListener("keypress", function (event) {  //añadir evento
-    if (event.key == "Enter") { //key del evento
-        let userInput = txtConversar.value;
-        switch (infoPersonaArray.length) { //condicional del evento
-            case 0:
-                conversar.innerHTML = "Hola " + userInput + " ¿Que edad tienes?"; //en el caso 1
-                txtConversar.placeholder = "Digita tu edad";
-                break;
-            case 1:
-                conversar.innerHTML = "¿Por que razón visitas este sitio?";  //en el caso 2
-                txtConversar.placeholder ="Explica tu motivo";
-                break;
-            case 2:
-                conversar.innerHTML = "¿De donde eres?";  //en el caso 3
-                txtConversar.placeholder ="Tu region";
-                break;
-        }
-        txtConversar.value = "";
-        infoPersonaArray.push(userInput);
-        if (infoPersonaArray.length == 4) { //pushear al array
-            let nombre = infoPersonaArray[0];
-            let edad = infoPersonaArray[1];
-            let razon = infoPersonaArray[2];
-            let region = infoPersonaArray[3];
-
-            infoPersona.push(new infoPersonaObject(nombre, edad, razon, region)); //crear objeto
-            alert(
-                "De acuerdo " +
-                infoPersonaArray[0] +
-                " ahora que te conozco, puedo contarte un poco sobre mi..."
-            );
-            infoPersonaArray = [];
-            txtConversar.placeholder = "¡Hola! ¿Como es tu nombre?";
-            console.log(infoPersona);
-        } else {
-            console.log(infoPersonaArray);
-        }
-        event.preventDefault(); //evitar recharge de pagina
+txtConversar.addEventListener("keypress", function (event) {
+  //añadir evento
+  if (event.key == "Enter") {
+    //key del evento
+    let userInput = txtConversar.value;
+    switch (
+      infoPersonaArray.length //condicional del evento
+    ) {
+      case 0:
+        conversar.innerHTML = "Hola " + userInput + " ¿Que edad tienes?"; //en el caso 1
+        txtConversar.placeholder = "Digita tu edad";
+        break;
+      case 1:
+        conversar.innerHTML = "¿Por que razón visitas este sitio?"; //en el caso 2
+        txtConversar.placeholder = "Explica tu motivo";
+        break;
+      case 2:
+        conversar.innerHTML = "¿De donde eres?"; //en el caso 3
+        txtConversar.placeholder = "Tu region";
+        break;
     }
+    txtConversar.value = "";
+    infoPersonaArray.push(userInput);
+    if (infoPersonaArray.length == 4) {
+      //pushear al array
+      let nombre = infoPersonaArray[0];
+      let edad = infoPersonaArray[1];
+      let razon = infoPersonaArray[2];
+      let region = infoPersonaArray[3];
+
+      infoPersona.push(new infoPersonaObject(nombre, edad, razon, region)); //crear objeto
+
+      conversar.innerHTML =
+        "De acuerdo " +
+        infoPersonaArray[0] +
+        " ahora que te conozco, puedo contarte un poco sobre mi...";
+      txtConversar.style.display = "none";
+
+      setTimeout(() => {
+        document.getElementById("conocer").style.opacity = 0;
+      }, 4000);
+
+      infoPersonaArray = [];
+      txtConversar.placeholder = "¡Hola! ¿Como es tu nombre?";
+      console.log(infoPersona);
+    } else {
+      console.log(infoPersonaArray);
+    }
+    event.preventDefault(); //evitar recharge de pagina
+  }
 });
